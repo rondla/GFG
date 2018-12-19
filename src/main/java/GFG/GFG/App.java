48ce925ee3c6f52a46fdcd1c0670f27a572f4448
kiran <saikiran.rondla@gmail.com>
2018-12-19 17:44:51 +0530

    Initial commit

---
+++ src/main/java/GFG/GFG/App.java
@@ -0,0 +1,13 @@
+package GFG.GFG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
